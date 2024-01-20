01dc16795ffd7595d4137e3f2ff0128a00cf7e3f
John Shaughnessy <john@johnshaughnessy.com>
2024-01-19 20:40:18 -0500

    Add websocket support. Echo received messages.

--- server.py
+++ server.py
@@ -2,22 +2,42 @@ print("Running server.py...")
 
 import eventlet
 import socketio
-from dotenv import load_dotenv
+import dotenv
 import os
+import json
+
+client_path = os.environ.get("CLIENT_PATH") or "browser-client/"
+http_bind_host = os.environ.get("HTTP_BIND_HOST") or "0.0.0.0"
+http_bind_port = int(os.environ.get("HTTP_BIND_PORT") or 5000)
+
+sio = socketio.Server(cors_allowed_origins="*")
+app = socketio.WSGIApp(sio, static_files={"/": client_path})
 
 def main():
-    if not load_dotenv():
+    if not dotenv.load_dotenv():
         print("Could not load .env file or it is empty. Please check if it exists and is readable.")
         exit(1)
 
-    client_path = os.environ.get('CLIENT_PATH') or "browser-client/"
-    http_bind_host = os.environ.get('HTTP_BIND_HOST') or "0.0.0.0"
-    http_bind_port = int(os.environ.get('HTTP_BIND_PORT') or 5000)
-
-    print("Starting server at http://" + http_bind_host + ":" + str(http_bind_port) + "...")
-    sio = socketio.Server(cors_allowed_origins='*')
-    app = socketio.WSGIApp(sio, static_files={'/': client_path})
     eventlet.wsgi.server(eventlet.listen((http_bind_host, http_bind_port)), app)
+
+@sio.event
+def connect(sid, environ):
+    print("connect ", sid)
+
+@sio.event
+def disconnect(sid):
+    print("disconnect ", sid)
+
+@sio.event
+def message(sid, data):
+    message = json.loads(data)
+
+    reply = {
+        "text": f"Ok, we received your message: {message.get('text')}",
+        "message_sid": message.get("message_sid"),
+        "isNewMessage": True,
+    }
+    sio.emit("message", json.dumps(reply), room=sid)
 
 if __name__ == "__main__":
     main()
