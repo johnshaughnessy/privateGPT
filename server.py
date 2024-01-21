d12baac7654a55d73493cac35c8096b9775a0b1d
John Shaughnessy <john@johnshaughnessy.com>
2024-01-21 17:08:28 -0500

    Add privateGPT-style querying

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 print("Running server.py...")
 
 import eventlet
@@ -5,6 +6,9 @@ import socketio
 import dotenv
 import os
 import json
+
+# The above imports are for libraries. In order to import files (like query.py), we write:
+import query
 
 client_path = os.environ.get("CLIENT_PATH") or "browser-client/"
 http_bind_host = os.environ.get("HTTP_BIND_HOST") or "0.0.0.0"
@@ -18,6 +22,8 @@ def main():
         print("Could not load .env file or it is empty. Please check if it exists and is readable.")
         exit(1)
 
+    query.init()
+
     eventlet.wsgi.server(eventlet.listen((http_bind_host, http_bind_port)), app)
 
 @sio.event
@@ -28,16 +34,36 @@ def connect(sid, environ):
 def disconnect(sid):
     print("disconnect ", sid)
 
+def generate_sid():
+    """Generate a random string of length 10."""
+    import random
+    import string
+    return ''.join(random.choice(string.ascii_letters) for i in range(10))
+
 @sio.event
 def message(sid, data):
-    message = json.loads(data)
+    # Random string
+    message_sid = generate_sid()
+    client_message = json.loads(data)
 
-    reply = {
-        "text": f"Ok, we received your message: {message.get('text')}",
-        "message_sid": message.get("message_sid"),
+    first_reply = {
+        "kind": "first_reply",
+        "text": f"Ok, we received your message: {client_message.get('text')}",
+        "message_sid": message_sid,
         "isNewMessage": True,
     }
-    sio.emit("message", json.dumps(reply), room=sid)
+
+    sio.emit("message", json.dumps(first_reply))
+
+    llm_output = query.query(client_message.get("text"))
+    second_reply = {
+        "kind": "second_reply",
+        "llm_output": llm_output,
+        "message_sid": message_sid,
+        "isNewMessage": True,
+    }
+
+    sio.emit("message", json.dumps(second_reply))
 
 if __name__ == "__main__":
     main()
